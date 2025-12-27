334c69cac34114e3d01a8cc35342b14ddf6bb3b6
Priyakumari2021 <dearpriya2017@gmail.com>
2025-12-27 16:27:19 +0100

    vue router without nuxt

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,32 @@
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { h } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'name',
+      component: HomeView
+    },
+    {
+      path: '/projects',
+      name: 'project',
+      component: () => import ('@/views/ProjectsView.vue')
+    },
+    {
+      path: '/projects/:id',
+      name: 'single-project',
+      component: () => import ('@/views/SingleProjectView.vue')
+    },
+    {
+      path: '/:catchAll(.*)*',
+      name: 'notFound',
+      component: h('p', { style: 'color: red;' }, '404 not found')
+    }
+    
+  ],
 })
 
 export default router
