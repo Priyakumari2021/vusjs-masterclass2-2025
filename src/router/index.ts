43520d968cd77feb01312e8442fff284db256f90
Priyakumari2021 <dearpriya2017@gmail.com>
2025-12-29 19:19:08 +0100

    some changes at Refactor Vue.js File based Routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,9 @@
-import HomeView from '@/views/HomeView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'name',
-      component: HomeView
-    },
-    {
-      path: '/projects',
-      name: 'project',
-      component: () => import ('@/views/ProjectsView.vue')
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import ('@/views/SingleProjectView.vue')
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'notFound',
-      component: h('p', { style: 'color: red;' }, '404 not found')
-    }
-    
-  ],
+  routes
 })
 
 export default router
